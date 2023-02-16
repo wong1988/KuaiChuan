cabfd5b19d842830de360a8524f4e8725033b125
wong1988 <wong1988@petalmail.com>
2023-02-16 14:08:25 +0800

    Update HttpRequest.java

--- transmit/src/main/java/io/github/wong1988/transmit/widget/server/HttpRequest.java
+++ transmit/src/main/java/io/github/wong1988/transmit/widget/server/HttpRequest.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 
 public class HttpRequest {
 
+    // 请求地址
     private String mUri;
+    // 请求方式
+    private String mType;
     private final HashMap<String, String> mHeaderMap = new HashMap<String, String>();
     private final Socket mSocket;
 
@@ -23,6 +26,14 @@ public class HttpRequest {
 
     public void setUri(String uri) {
         this.mUri = uri;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String mType) {
+        this.mType = mType;
     }
 
     public void addHeader(String key, String value) {
