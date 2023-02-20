a63f5c3ee5b032e018f4400bf5fe759c4823031b
wong1988 <wong1988@petalmail.com>
2023-02-20 17:14:06 +0800

    Update ImageFragment.java

--- transmit/src/main/java/io/github/wong1988/transmit/fragment/ImageFragment.java
+++ transmit/src/main/java/io/github/wong1988/transmit/fragment/ImageFragment.java
@@ -5,12 +5,16 @@ import android.view.View;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
 
 import java.util.List;
 
+import io.github.wong1988.adapter.divider.GridLayoutManagerDivider;
+import io.github.wong1988.adapter.listener.OnItemClickListener;
 import io.github.wong1988.kit.entity.FileInfo;
 import io.github.wong1988.kit.task.FileAsyncTask;
 import io.github.wong1988.kit.utils.FileUtils;
+import io.github.wong1988.kit.utils.UiUtils;
 import io.github.wong1988.media.MediaCenter;
 import io.github.wong1988.transmit.R;
 import io.github.wong1988.transmit.adapter.MediaAdapter;
@@ -30,6 +34,8 @@ public class ImageFragment extends BaseListFragment<FileInfo, MediaAdapter> {
     @Override
     protected void init(View rootView) {
         rv = rootView.findViewById(R.id.wong_rv);
+        // 解决刷新闪烁
+        ((SimpleItemAnimator) rv.getItemAnimator()).setSupportsChangeAnimations(false);
     }
 
     @Override
@@ -54,7 +60,7 @@ public class ImageFragment extends BaseListFragment<FileInfo, MediaAdapter> {
 
     @Override
     protected RecyclerView.ItemDecoration initItemDecoration() {
-        return null;
+        return GridLayoutManagerDivider.getVerticalDivider(getResources().getColor(android.R.color.transparent), (int) UiUtils.dip2px(2));
     }
 
     @Override
@@ -64,7 +70,13 @@ public class ImageFragment extends BaseListFragment<FileInfo, MediaAdapter> {
 
     @Override
     protected void initListener() {
-
+        mAdapter.setOnItemClickListener(new OnItemClickListener<FileInfo>() {
+            @Override
+            public void onClick(FileInfo fileInfo, int position, View view) {
+                fileInfo.setExtra("true".equals(fileInfo.getExtra()) ? "" : "true");
+                mAdapter.notifyItemChanged(mAdapter.getRealPosition(position));
+            }
+        });
     }
 
     @SuppressLint("MissingPermission")
